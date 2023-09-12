8d33a5c38f25bba08ccb4aa1cee71d2f742ce42d
Carlos Pacheco <113793302+crpcpacheco1703@users.noreply.github.com>
2023-09-11 20:08:43 -0600

    Update Ejercicio04.cpp
    Ejercicio 4

--- HojaDeTrabajo01/Ejercicio04.cpp
+++ HojaDeTrabajo01/Ejercicio04.cpp
@@ -1,6 +1,16 @@
 #include "Ejercicio04.h"
 
-vector<string> Ejercicio04::generateParenthesis(int n)
+vector<string> Ejercicio04::generateParenthesis(int n, string combinacion, int abrir, in cerrar, std::vector<std::string>& resultado)
 {
-    return vector<string>();
+    if (abrir == 0 && cerrar == 0) {
+        resultado.push_back(combinacion);
+        return;
+    }
+    if (abrir > 0) {
+        generateParenthesis(combinacion + "(", abrir -1, cerrar, resultado);
+    }
+    if (cerrar > abrir) {
+        generatePatenthesis(combinacion + ")", abrir, cerrar -1, resultado);
+    }
 }
+
