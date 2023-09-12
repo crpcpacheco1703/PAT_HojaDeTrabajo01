df20de0d9162485bb9cd0d45cf52926013d94a2d
Carlos Pacheco <113793302+crpcpacheco1703@users.noreply.github.com>
2023-09-11 19:49:22 -0600

    Update Ejercicio02.cpp
    Ejercicio 2

--- HojaDeTrabajo01/Ejercicio02.cpp
+++ HojaDeTrabajo01/Ejercicio02.cpp
@@ -1,6 +1,18 @@
 #include "Ejercicio02.h"
 
-bool Ejercicio02::isPalindrome(string& s)
+bool Ejercicio02::isPalindrome(string& s, int inicio, int fin)
 {
-    return false;
+    if (inicio >= fin){
+        return true;
+    }
+    else if (s[inicio] == s[fin]){
+        return isPalindrome(s, inicio + 1, fin - 1);
+    }
+    else {
+        return false;
+    }
 }
+bool isPalindrome(const std::string& s) {
+    return isPalindrome(s, 0, s.size() - 1);
+}
+
